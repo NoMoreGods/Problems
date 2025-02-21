c0ba90dace4b870fe391f7ee53ef7e61cf82e44d
Mark Novikov <61317452+NoMoreGods@users.noreply.github.com>
2025-02-21 13:31:04 +0300

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,8 +1,8 @@
 //Что выведет Promise
 Promise.resolve(1)
     .then(x=>x+1) //1+1 = 2
-    .then(x=>x+'str') // 2+'str' ='2str'
-    .then(x=>{throw x}) // выбрасывает ошибку
+    .then(x=>x+'str') // '2str'
+    .then(x=>{throw x}) // выбрасывает ошибку'2str'
     .then(x=>x+3) // игнорируем
     .then(x=>console.log(x)) // игнорируем
     .catch(err => console.log(err)) //console.log выведет ошибку '2str'
